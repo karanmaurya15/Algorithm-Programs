b47e500bdf275b4171124ffbb0785cb3b1f05175
karanmaurya15 <karanmaurya9663@gmail.com>
2023-02-02 22:28:02 +0530

    [Karan] Integers prints them in sorted order using Bubble Sort

---
+++ Algorithm-Programs/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    internal class BubbleSort
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After sorting: ");
+            Display(array);
+        }
+        public void Display(int[] array)
+        {
+            foreach (var data in array)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
--- Algorithm-Programs/Program.cs
+++ Algorithm-Programs/Program.cs
@@ -1,16 +1,19 @@
-﻿namespace Algorithm_Programs
+﻿using System;
+
+namespace Algorithm_Programs
 {
     internal class Program
     {
         public static string binaryTextPath = @"C:\BridgeLabz\Algorithm-Programs\Algorithm-Programs\Files\BinarySearch.txt";
         static void Main(string[] args)
         {
+            int[] array = { 5, 1, 8, 7, 2, 4, 6, 9, 3, 10 };
             Console.WriteLine("Welcome to the Algorithm Programs");
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.BinarySearch \n 2.InsertionSort \n 3.Exit");
-                Console.Write("Select any one from the above option: ");
+                Console.WriteLine(" 1.BinarySearch \n 2.InsertionSort \n 3.BubbleSort \n 4.Exit");
+                Console.Write("\nSelect any one from the above option: ");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -25,6 +28,12 @@
                         sort.Sort();
                         break;
                     case 3:
+                        BubbleSort bubblesort = new BubbleSort();
+                        Console.WriteLine("Before sorting: ");
+                        bubblesort.Display(array);
+                        bubblesort.Sort(array);
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
