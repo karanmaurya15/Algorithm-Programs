a4addd0596f05967865d930808592c9b1d76ed15
karanmaurya15 <karanmaurya9663@gmail.com>
2023-02-02 21:32:32 +0530

    [Karan] Binary Search the Word from Word List

---
+++ Algorithm-Programs/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    internal class BinarySearch
+    {
+        string[] binaryArray;
+        public void ReadFile(string filepath)
+        {
+            string words = File.ReadAllText(filepath);
+            binaryArray = words.Split(",");
+        }
+        public void BinarySearchWord(string search)
+        {
+            foreach (var word in binaryArray)
+            {
+                if (word.Equals(search))
+                {
+                    Console.WriteLine("Word " + search + " is found in the text file");
+                    return;
+                }
+            }
+            Console.WriteLine("Word " + search + " is not found in the given text file");
+        }
+    }
+}
+
--- Algorithm-Programs/Program.cs
+++ Algorithm-Programs/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        public static string binaryTextPath = @"C:\BridgeLabz\Algorithm-Programs\Algorithm-Programs\Files\BinarySearch.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Algorithm Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.BinarySearch \n 2.Exit");
+                Console.Write("Select any one from the above option: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        BinarySearch search = new BinarySearch();
+                        search.ReadFile(binaryTextPath);
+                        search.BinarySearchWord("problem");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
