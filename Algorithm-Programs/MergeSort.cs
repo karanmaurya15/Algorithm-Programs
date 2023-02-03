4294dcfad8c92a7c11e3c768ff091f415ce984e1
karanmaurya15 <karanmaurya9663@gmail.com>
2023-02-03 21:38:28 +0530

    [Karan] Integers prints them in sorted order using Merge Sort

---
+++ Algorithm-Programs/MergeSort.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    internal class MergeSort
+    {
+        public void Merge_Sort(int[] array, int lo, int hi)//lo-lower index, hi-higher index
+        {
+            if (lo < hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+
+                Merge_Sort(array, lo, mid);
+                Merge_Sort(array, mid + 1, hi);
+                SortTwoArrays(array, lo, mid, hi);
+            }
+        }
+        public void SortTwoArrays(int[] array, int lo, int mid, int hi)
+        {
+            int n1 = mid - lo + 1;
+            int n2 = hi - mid;
+            int[] leftArray = new int[n1];
+            int[] rightArray = new int[n2];
+            int i, j;
+            for (i = 0; i < n1; i++)
+            {
+                leftArray[i] = array[lo + i];
+            }
+            for (j = 0; j < n2; j++)
+            {
+                rightArray[j] = array[mid + 1 + j];
+            }
+            i = 0; j = 0;
+            int k = lo;
+            while (i < n1 && j < n2)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                array[k] = leftArray[i];
+                i++; k++;
+            }
+            while (j < n2)
+            {
+                array[k] = rightArray[j];
+                j++; k++;
+            }
+        }
+        public void Display(int[] array)
+        {
+            foreach (var data in array)
+                Console.WriteLine(data);
+        }
+    }
+}
--- Algorithm-Programs/Program.cs
+++ Algorithm-Programs/Program.cs
@@ -8,11 +8,12 @@ namespace Algorithm_Programs
         static void Main(string[] args)
         {
             int[] array = { 5, 1, 8, 7, 2, 4, 6, 9, 3, 10 };
+            int[] ary = { 26, 20, 54, 34, 12, 39, 19, 45 };
             Console.WriteLine("Welcome to the Algorithm Programs");
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine(" 1.BinarySearch \n 2.InsertionSort \n 3.BubbleSort \n 4.Exit");
+                Console.WriteLine(" 1.BinarySearch \n 2.InsertionSort \n 3.BubbleSort \n 4.MergeSort \n 5.Exit");
                 Console.Write("\nSelect any one from the above option: ");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
@@ -34,6 +35,14 @@ namespace Algorithm_Programs
                         bubblesort.Sort(array);
                         break;
                     case 4:
+                        MergeSort mergeSort = new MergeSort();
+                        Console.WriteLine("Array Before Sorting: ");
+                        mergeSort.Display(ary);
+                        mergeSort.Merge_Sort(ary, 0, ary.Length - 1);
+                        Console.WriteLine("Array after Sorting: ");
+                        mergeSort.Display(ary);
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
